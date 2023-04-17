9ac2a92a395fb7b0990ee03b7b511bfc214c2ece
Farhan <farhan.farooq@live.com>
2023-04-18 02:43:29 +0500

    changed corsmiddleware import

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from fastapi_login import LoginManager
 import os
 from dotenv import load_dotenv
